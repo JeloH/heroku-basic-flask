d25179721b4056003e336873a104e86afdee2ac2
Hamed <hajelovdar@yahoo.com>
2023-05-10 12:06:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,11 @@ import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
-
 @app.route("/predict")
 def predict():
-    vol_moving_avg = request.args.get('vol_moving_avg')
-    print(vol_moving_avg)
-    adj_close_rolling_med = request.args.get('adj_close_rolling_med')
-    print(adj_close_rolling_med)
-    adj_close_rolling_med=3.14
-    features = [np.array([vol_moving_avg, adj_close_rolling_med])]
-    y_pred = model.predict(features)
+    a1 = int(request.args.get('vol_moving_avg'))
+    a2 = int(request.args.get('adj_close_rolling_med'))
+    y_pred = model.predict(np.array([[a2, a1]]))
     test1=str(y_pred)
     print(y_pred)
     return test1
